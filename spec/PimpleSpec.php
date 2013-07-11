b5a5d87cc9005bfa2ac6a7ddd48f2b555df48803
Luis Cordova <cordoval@gmail.com>
2013-07-11 13:47:33 -0500

    failing test for isset

--- spec/PimpleSpec.php
+++ spec/PimpleSpec.php
@@ -4,6 +4,7 @@ namespace spec;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use ReflectionFunction;
 
 class PimpleSpec extends ObjectBehavior
 {
@@ -13,9 +14,55 @@ class PimpleSpec extends ObjectBehavior
         $this->shouldImplement('ArrayAccess');
     }
 
-    function it_should_store_strings()
+    function it_stores_strings()
     {
         $this['param'] = 'value';
         $this['param']->shouldReturn('value');
+    }
+
+    function it_stores_executable_closures()
+    {
+        $this['service'] = function() { return new \SplObjectStorage(); };
+
+        $this->trueOffsetGet('service')->shouldHaveType('SplObjectStorage');
+    }
+
+    function it_nests_an_offsetGet_method_to_avoid_collisions_with_PHPSpec()
+    {
+        $this['service'] = function() { return new \SplObjectStorage(); };
+
+        $this->offsetGet('service')->shouldBeLike($this->trueOffsetGet('service'));
+    }
+
+    function it_executes_with_container_argument_when_value_is_a_factory()
+    {
+        $this['container'] = function($container) { return $container; };
+        $this->executeFactory('container')->shouldReturn($this);
+    }
+
+    public function it_responds_to_isset()
+    {
+        $this['param'] = 'value';
+        $this['service'] = function () { return new \SplObjectStorage(); };
+        $this['null'] = null;
+
+        $this->shouldHaveKey('param');
+        $this->shouldHaveKey('service');
+        $this->shouldHaveKey('null');
+        $this->shouldNotHaveKey('non_existent');
+    }
+
+    public function getMatchers()
+    {
+        return [
+            'beAClosure' => function($subject, $closure) {
+                $rf = new ReflectionFunction($closure);
+                $result = call_user_func($closure) === call_user_func($subject);
+                return $rf->isClosure() && $result;
+            },
+            'returnWhenInvokedWith' => function($subject, array $resultAndArgument) {
+                return $resultAndArgument[0] === call_user_func($subject, $resultAndArgument[1]);
+            }
+        ];
     }
 }
--- src/Pimple.php
+++ src/Pimple.php
@@ -16,11 +16,25 @@ class Pimple implements ArrayAccess
 
     public function offsetExists($offset)
     {
-
+        throw new \Exception('Iam invoked with '.$offset);
     }
 
     public function offsetGet($offset)
     {
+        return $this->trueOffsetGet($offset);
+    }
+
+    public function trueOffsetGet($offset)
+    {
+        if (is_object($this->collection[$offset]) && method_exists($this->collection[$offset], '__invoke')) {
+            return $this->executeFactory($offset);
+        }
+
         return $this->collection[$offset];
+    }
+
+    public function executeFactory($offset)
+    {
+        return $this->collection[$offset]($this);
     }
 }
